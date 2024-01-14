6695f20495d6764cc320a9d76122745276aef490
aliozben <aliozben4@gmail.com>
2024-01-14 15:06:05 +0300

    feat: Register endpoint has been added.

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt";
+import {NextRequest, NextResponse} from "next/server";
+import prisma from "@/lib/db";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const {username, password} = body;
+  console.log(body);
+  if (!username || !password) {
+    return NextResponse.json(
+      {
+        message: "Username and password are required",
+      },
+      {status: 400}
+    );
+  }
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (isUserExist) {
+    return NextResponse.json(
+      {
+        message: "User already exists",
+      },
+      {status: 400}
+    );
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      password: hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
+}
