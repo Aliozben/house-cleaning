05a80780ddb460956a07a776f3250dd61b996d9e
aliozben <aliozben4@gmail.com>
2024-01-19 15:33:36 +0300

    fix: Added address box to survey information form.

--- src/app/dashboard/surveys/[id]/page.tsx
+++ src/app/dashboard/surveys/[id]/page.tsx
@@ -214,7 +214,20 @@ export default async function SurveyDetailsPage({
             defaultStatus={survey.renovation}
             readonly
           />
+          <div className="col-span-4">
+            <label className="block text-sm font-semibold leading-6 text-gray-900">
+              Address
+            </label>
 
+            <div className="mt-2.5">
+              <textarea
+                value={survey.address}
+                readOnly
+                rows={4}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
           {survey.additionalDetails && (
             <div className="col-span-4">
               <label className="block text-sm font-semibold leading-6 text-gray-900">
