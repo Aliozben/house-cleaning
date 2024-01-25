4499e1802a3fdc84b9a981fab4b63606d662c423
aliozben <aliozben4@gmail.com>
2024-01-25 15:45:30 +0300

    feat: Protection has been added for private endpoints.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,19 @@
-export {default} from "next-auth/middleware";
+import {NextRequest, NextResponse} from "next/server";
+import {getToken} from "next-auth/jwt";
 
-export const config = {matcher: ["/dashboard"]};
+export const config = {matcher: ["/dashboard", "/api/survey", "/api/register"]};
+
+export async function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname === "/api/survey" && req.method === "POST") {
+    return NextResponse.next();
+  }
+
+  const session = await getToken({req});
+  if (!session) {
+    return Response.json(
+      {success: false, message: "authentication failed"},
+      {status: 401}
+    );
+  }
+  return NextResponse.next();
+}
