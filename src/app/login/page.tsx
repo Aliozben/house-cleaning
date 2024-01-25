46bf584be0139b38f676c0d6cc84e203919f6ee4
aliozben <aliozben4@gmail.com>
2024-01-25 14:01:40 +0300

    fix: Trying route to dashboard without waiting response for authenticate.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,13 @@
 
 import {useState} from "react";
 import {useRouter} from "next/navigation";
-import {signIn} from "next-auth/react";
+import {signIn, useSession} from "next-auth/react";
 
 export default function LoginPage() {
   const router = useRouter();
+  const {status} = useSession();
+  if (status === "authenticated") router.push("/dashboard");
+
   const [data, setData] = useState<{
     username: string;
     password: string;
@@ -13,7 +16,7 @@ export default function LoginPage() {
 
   const loginUser = async (e: any) => {
     e.preventDefault();
-    signIn("credentials", {
+    await signIn("credentials", {
       ...data,
       redirect: false,
     });
@@ -25,11 +28,6 @@ export default function LoginPage() {
       {" "}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
@@ -65,14 +63,6 @@ export default function LoginPage() {
                 >
                   Password
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <input
@@ -96,16 +86,6 @@ export default function LoginPage() {
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Start a 14 day free trial
-            </a>
-          </p>
         </div>
       </div>
     </>
