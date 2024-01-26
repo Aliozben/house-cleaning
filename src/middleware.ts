d8c356023886aa8a90f5a079f3719e70dba9c265
aliozben <aliozben4@gmail.com>
2024-01-26 13:54:50 +0300

    doc: Added log to middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,14 +7,18 @@ export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === "/api/survey" && req.method === "POST") {
     return NextResponse.next();
   }
-
-  const session = await getToken({req});
-  console.log("session", session);
-  if (!session) {
-    return Response.json(
-      {success: false, message: "authentication failed"},
-      {status: 401}
-    );
+  try {
+    const session = await getToken({req});
+    console.log("session", session);
+    if (!session) {
+      return Response.json(
+        {success: false, message: "authentication failed"},
+        {status: 401}
+      );
+    }
+    return NextResponse.next();
+  } catch (error) {
+    console.log("middleware error", error);
+    return NextResponse.json({success: false, message: "error"}, {status: 401});
   }
-  return NextResponse.next();
 }
