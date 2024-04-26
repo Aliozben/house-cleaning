bc36f3e611e777c47512cac538b9478506bfd344
aliozben <aliozben4@gmail.com>
2024-04-26 13:54:58 +0300

    chore: Added log to survey endpoint.

--- src/app/api/survey/route.ts
+++ src/app/api/survey/route.ts
@@ -2,13 +2,17 @@ import {NextResponse} from "next/server";
 import prisma from "@/lib/db";
 
 export async function GET() {
-  const surveys = await prisma.survey.findMany({
-    select: {
-      id: true,
-      nameInfo: true,
-      lastReadDate: true,
-    },
-  });
-
-  return NextResponse.json(surveys);
+  try {
+    const surveys = await prisma.survey.findMany({
+      select: {
+        id: true,
+        nameInfo: true,
+        lastReadDate: true,
+      },
+    });
+    console.log("[api/survey] GET surverys:", surveys);
+    return NextResponse.json(surveys);
+  } catch (error) {
+    console.error("[api/survey] GET error:", error);
+  }
 }
