b49feeb5b1813684ad044ad7992d9461078e4220
aliozben <aliozben4@gmail.com>
2024-12-13 23:33:51 +0300

    fix: forcing endpoint to be dynamic

--- src/app/api/survey/route.ts
+++ src/app/api/survey/route.ts
@@ -1,6 +1,7 @@
 import {NextResponse} from "next/server";
 import prisma from "@/lib/db";
 
+export const dynamic = "force-dynamic";
 export async function GET() {
   try {
     const surveys = await prisma.survey.findMany({
